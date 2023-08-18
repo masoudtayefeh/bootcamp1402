3032ca4c37c30c26291822e7cdd53251a9b417a0
masoudtayefeh <masoudtayefeh@yahoo.com>
2023-08-18 13:43:51 +0330

    spark mongo stream added

---
+++ spark/pipeline_2.py
@@ -0,0 +1,67 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, IntegerType, TimestampType
+from pymongo import MongoClient
+import datetime
+
+
+class MongoUpsert:
+    def __init__(self):
+        self.mongo_client = MongoClient("mongodb://localhost:27017/")
+        self.db = self.mongo_client["bootcamp"]
+        self.collection = self.db["eligible_customers"]
+        self.threshold_date = datetime.datetime(2023, 1, 1)
+        self.threshold_balance = 90_000_000
+
+    def upsert_to_mongo(self, batch_df, batch_id):
+        for row in batch_df.rdd.toLocalIterator():
+            user_id = row['value']["user_id"]
+            account_created_date = row['value']["account_created_date"]
+            account_balance = row['value']["account_balance"]
+            
+            if account_created_date < self.threshold_date or account_balance < self.threshold_balance:
+                continue
+
+            filter_query = {"user_id": user_id}
+            update_query = {
+                "$set": {
+                    "account_created_date": account_created_date,
+                    "account_balance": account_balance
+                }
+            }
+
+            self.collection.update_one(filter_query, update_query, upsert=True)
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+        .appName("Pipeline 2") \
+        .master("local[3]") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1") \
+        .getOrCreate()
+
+    schema = StructType(
+        [
+            StructField("user_id", IntegerType()),
+            StructField("account_created_date", TimestampType()),
+            StructField("account_balance", IntegerType())
+        ]
+    )
+    kafka_df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "account_balance") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    value_df = kafka_df.select(from_json(col("value").cast("string"), schema).alias("value"))
+
+    mongo_upsert = MongoUpsert()
+
+    query = value_df.writeStream \
+        .foreachBatch(mongo_upsert.upsert_to_mongo) \
+        .outputMode("update") \
+        .trigger(processingTime="3 second") \
+        .start()
+
+    query.awaitTermination()
---
+++ spark/producer.py
@@ -0,0 +1,64 @@
+import random
+from typing import List, Dict
+from confluent_kafka import Producer
+import json
+from randomtimestamp import randomtimestamp
+from confluent_kafka.admin import AdminClient, NewTopic
+
+
+kafka_server = {"bootstrap.servers": "127.0.0.1:9092"}
+admin_client = AdminClient(kafka_server)
+
+
+def topic_creator(topic_details: List[Dict]):
+    topic_list = [NewTopic(topic=topic['name'], num_partitions=topic['partition']) for topic in topic_details]
+    print(topic_list)
+    fs = admin_client.create_topics(topic_list)
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} created".format(topic))
+        except Exception as e:
+            print("Failed to create topic {}: {}".format(topic, e))
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        pass
+        print('Message delivered to {}'.format(msg.topic()))
+
+
+def confluent_kafka_producer():
+    producer = Producer(kafka_server)
+    for user_id in range(1, 1001):
+        record_key = user_id
+        account_created_date = randomtimestamp(
+            start_year=2020, end_year=2023, pattern="%Y-%m-%d %H:%M:%S")
+        account_balance = random.randint(0, 100_000_000)
+        record_value = {
+            "user_id": user_id,
+            "account_created_date": account_created_date,
+            "account_balance": account_balance
+        }
+
+        record_value_json = json.dumps(record_value, default=str)
+
+        producer.produce(
+            "account_balance",
+            key=str(record_key),
+            value=record_value_json,
+            on_delivery=delivery_report
+        )
+
+    producer.flush()
+
+
+topic_configs = [
+    {'name': 'account_balance', 'partition': 3},
+]
+topic_creator(topic_configs)
+confluent_kafka_producer()
